00641f741449d7a77d4b04c4f50a9f15a9524662
antisocialcoolkid <antisocialcoolkid@outlook.cl>
2026-08-15 11:26:40 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,11 @@
-const SUPABASE_URL = "https://qvqhanioapefeyxtbzlp.supabase.co";
+const SUPABASE_URL =
+    "https://qvqhanioapefeyxtbzlp.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_hJVj-2VGj2hEXUgc91QY1Q_zGjw9qp1";
+const SUPABASE_KEY =
+    "sb_publishable_hJVj-2VGj2hEXUgc91QY1Q_zGjw9qp1";
 
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
