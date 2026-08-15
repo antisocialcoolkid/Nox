c0714a7e46ab4f5bf874e8c54ae953b4e5d7eba2
antisocialcoolkid <antisocialcoolkid@outlook.cl>
2026-08-15 10:47:28 -0400

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://qvqhanioapefeyxtbzlp.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_hJVj-2VGj2hEXUgc91QY1Q_zGjw9qp1";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
